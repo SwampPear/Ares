04969cfdd5d04d928c0d338a55b70de3065b4258
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-06-26 20:24:56 -0400

    basic django project set up

--- ares/ares/settings.py
+++ ares/ares/settings.py
@@ -36,7 +36,9 @@ ROOT_URLCONF = 'ares.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            Path(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,5 +84,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
